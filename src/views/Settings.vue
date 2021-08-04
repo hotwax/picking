049afe4208fa2a40eb136feaef738faa3ea0f6d5
Azkya Khan <azkya.khan@hotwax.co>
2021-08-04 22:13:54 +0530

    Created static UI of settings page (#51hf6z)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -5,25 +5,43 @@
         <ion-title>{{ $t("Settings") }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    
     <ion-content :fullscreen="true">
-      
+      <ion-item>
+        <ion-icon :icon="businessOutline" slot="start" />
+        <ion-label>Store</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-icon :icon="personCircleOutline" slot="start" />
+        <ion-label>user name</ion-label>
+        <ion-button fill="outline">Logout</ion-button>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { businessOutline, personCircleOutline } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'Settings',
   components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
+    IonButton, 
+    IonContent, 
+    IonHeader, 
+    IonIcon, 
+    IonItem, 
+    IonLabel, 
+    IonPage, 
+    IonTitle, 
     IonToolbar
+  },
+  setup(){
+    return {
+      businessOutline, 
+      personCircleOutline
+    }
   }
 });
 </script>
