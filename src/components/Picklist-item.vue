c7b7e08285691d67c32cdb5cb780e3efd72e25d3
Yash Maheshwari <yash.maheshwari@hotwax.co>
2021-08-06 17:10:29 +0530

    Added: code to format date and implementing routing(#5cwadk)

--- src/components/Picklist-item.vue
+++ src/components/Picklist-item.vue
@@ -1,13 +1,14 @@
 <template>
-  <ion-item v-for="picklist in pickingList" :key="picklist.picklistId" detail>
+  <ion-item v-for="picklist in pickingList" :key="picklist.picklistId" @click="viewPicklist(picklist)" detail>
     <ion-label>{{ user.partyName }}</ion-label>
-    <ion-note slot="end">{{ picklist.picklistDate }}</ion-note>
+    <ion-note slot="end">{{ $filters.formatDate(picklist.picklistDate) }}</ion-note>
   </ion-item>
 </template>
 
 <script lang="ts">
 import { IonItem, IonLabel, IonNote } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 import { mapGetters } from 'vuex';
 
 export default defineComponent({
@@ -22,6 +23,18 @@ export default defineComponent({
     ...mapGetters({
       user: 'user/getUserProfile'
     })
+  },
+  methods: {
+    viewPicklist (picklist: any) {
+      this.router.push({ path: `/picklist-details/${picklist.picklistId}` });
+    }
+  },
+  setup () {
+    const router = useRouter();
+
+    return {
+      router
+    }
   }
 });
 </script>
