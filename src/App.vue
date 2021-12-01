5945b011ca90abc69daf7e90c03b7c2436fbe868
Azkya Khan <azkya.khan@hotwax.co>
2021-12-01 19:12:27 +0530

    Removed filter from picklist page (#1vrhjph)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
   <ion-app>
-    <ion-menu side="end" type="overlay" menu-id="filter" content-id="main">
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>{{ $t("Filters") }}</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <ion-list>
-          <ion-item> {{ $t("Only show my picklists") }}
-            <ion-toggle slot="end"></ion-toggle>
-          </ion-item>
-          <ion-item>{{ $t("Hide completed picklists") }}
-            <ion-toggle slot="end"></ion-toggle>
-          </ion-item>
-        </ion-list>
-      </ion-content>
-    </ion-menu>
-    <ion-router-outlet id="main" />
+    <ion-router-outlet />
   </ion-app>
 </template>
 
 <script lang="ts">
-import { IonApp, IonContent, IonHeader, IonItem, IonList, IonMenu, IonRouterOutlet, IonTitle, IonToggle, IonToolbar } from '@ionic/vue';
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { loadingController } from '@ionic/vue';
 import emitter from "@/event-bus"
@@ -31,16 +14,8 @@ import emitter from "@/event-bus"
 export default defineComponent({
   name: 'App',
   components: {
-    IonApp, 
-    IonContent, 
-    IonHeader, 
-    IonItem, 
-    IonList, 
-    IonMenu, 
-    IonRouterOutlet,
-    IonTitle, 
-    IonToggle, 
-    IonToolbar
+    IonApp,  
+    IonRouterOutlet
   },
   data() {
     return {
