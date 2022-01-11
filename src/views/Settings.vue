0891e0807c1979809013726fde73fdce06b084f0
Aditya Sharma <iamadityasharma7@gmail.com>
2022-01-11 12:12:02 +0530

    Fixed spacing

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -16,7 +16,7 @@
       <ion-item>
         <ion-icon :icon="codeWorkingOutline" slot="start"/>
         <ion-label>{{ $t("OMS") }}</ion-label>
-        <ion-note slot="end">{{instanceUrl}}</ion-note>
+        <ion-note slot="end">{{ instanceUrl }}</ion-note>
       </ion-item>
       <ion-item>
         <ion-icon :icon="personCircleOutline" slot="start" />
