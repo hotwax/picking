0731197b7fd56093c3bda746ccf5c19c0f122e9e
Disha <dishatalreja1202@gmail.com>
2022-02-09 17:03:53 +0530

    Implemented: encryption and decryption of data(#207jut0)

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,12 +7,14 @@ import createPersistedState from "vuex-persistedstate";
 import userModule from './modules/user';
 import picklistModule from "./modules/picklist"
 import productModule from "./modules/product"
+import SecureLS from "secure-ls";
 
 
 // TODO check how to register it from the components only
 // Handle same module registering multiple time on page refresh
 //store.registerModule('user', userModule);
 
+const ls = new SecureLS({ encodingType: 'aes' , isCompression: true , encryptionSecret: process.env.VUE_APP_SECURITY_KEY});
 
 const state: any = {
 
@@ -20,7 +22,20 @@ const state: any = {
 
 const persistState = createPersistedState({
     paths: ['user', 'product'],
-    fetchBeforeUse: true
+    fetchBeforeUse: true,
+    storage: {
+      getItem: key => {
+        try {
+          return ls.get(key)
+        } 
+        catch(err) {
+          ls.remove(key)
+            return ls.get(key)
+        }
+      },       
+      setItem: (key, value) => ls.set(key, value),
+      removeItem: key => ls.remove(key)
+    }
 })
 
 // Added modules here so that hydration takes place before routing
