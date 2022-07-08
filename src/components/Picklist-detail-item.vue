6065eefdd4cd8be6899264a455e933089047881c
Yash Maheshwari <yash.maheshwari@hotwax.co>
2022-07-08 19:10:35 +0530

    Improved: the detail item component as per the updated schema for product(#2hxx8va)

--- src/components/Picklist-detail-item.vue
+++ src/components/Picklist-detail-item.vue
@@ -1,14 +1,15 @@
 <template>
   <ion-item v-bind:key="picklist.productId" v-for="picklist in picklists"  @click="picklist.isChecked = !picklist.isChecked" lines="none" >
     <ion-thumbnail slot="start">
-      <Image :src="getProduct(picklist.productId).mainImageUrl" />
+      <Image :src="getProduct(picklist.productId).images?.mainImageUrl" />
     </ion-thumbnail>  
     <ion-label>
+      <!-- TODO: not getting parentProductName in the response, update this when response is updated-->
       <p class="caption">{{ getProduct(picklist.productId).parentProductName}}</p>
       <h2>{{ getProduct(picklist.productId).productName}}</h2>
       <h2>{{ picklist.productId }}</h2>
-      <p>{{ $t("Color") }} : {{ $filters.getFeatures(getProduct(picklist.productId).featureHierarchy, '1/COLOR/') }}</p>
-      <p>{{ $t("Size") }} : {{ $filters.getFeatures(getProduct(picklist.productId).featureHierarchy, '1/SIZE/') }}</p>
+      <p v-if="getFeature(getProduct(picklist.productId).features, 'Color')">{{ $t("Color") }} : {{ getFeature(getProduct(picklist.productId).features, 'Color') }}</p>
+      <p v-if="getFeature(getProduct(picklist.productId).features, 'Size')">{{ $t("Size") }} : {{ getFeature(getProduct(picklist.productId).features, 'Size') }}</p>
     </ion-label>
     <ion-checkbox :modelValue="picklist.isChecked" slot="end" />
   </ion-item>
@@ -19,6 +20,7 @@ import { IonCheckbox, IonItem, IonLabel, IonThumbnail } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { mapGetters, useStore } from 'vuex';
 import Image from '@/components/Image.vue'
+import { getFeature } from '@/utils'
 
 export default defineComponent({
   name: 'PicklistDetailItem',
@@ -39,6 +41,7 @@ export default defineComponent({
   setup() {
     const store = useStore(); 
     return {
+      getFeature,
       store
     }
   }
