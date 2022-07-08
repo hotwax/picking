cb38a79f1e4a105df16f7049f9a8e00699029830
Yash Maheshwari <yash.maheshwari@hotwax.co>
2022-07-08 19:11:36 +0530

    Improved: the product action to use the oms package method for fetching the product details(#2hxx8va)

--- src/store/modules/product/actions.ts
+++ src/store/modules/product/actions.ts
@@ -2,8 +2,8 @@ import { ActionTree } from 'vuex'
 import RootState from '@/store/RootState'
 import ProductState from './ProductState'
 import * as types from './mutation-types'
-import { ProductService } from '@/services/ProductService'
-import { hasError } from '@/utils'
+import { fetchProducts } from '@hotwax/oms-api/src/product'
+import { isError } from '@hotwax/oms-api/src/util'
 
 const actions: ActionTree<ProductState, RootState> = {
 
@@ -12,27 +12,29 @@ const actions: ActionTree<ProductState, RootState> = {
    */
   async fetchProducts ( { commit, state }, { productIds }) {
     const cachedProductIds = Object.keys(state.cached);
-    const productIdFilter= productIds.reduce((filter: string, productId: any) => {
-      if (filter !== '') filter += ' OR '
-      // If product already exist in cached products skip
-      if (cachedProductIds.includes(productId)) {
-        return filter;
-      } else {
-        return filter += productId;
+    const productIdFilter= productIds.reduce((filter: Array<any>, productId: any) => {
+      // If product does not exist in cached products then add the id
+      if (!cachedProductIds.includes(productId) && productId) {
+        filter.push(productId);
       }
-    }, '');
+      return filter;
+    }, []);
 
-    // If there are no products skip the API call
-    if (productIdFilter === '') return;
+    // If there are no product ids to search skip the API call
+    if (productIdFilter.length <= 0) return;
 
-    const resp = await ProductService.fetchProducts({
-      "filters": ['productId: (' + productIdFilter + ')'],
-      "viewSize": productIds.length
+    const resp = await fetchProducts({
+      filters: { 'productId': productIdFilter },
+      viewSize: productIdFilter.length,
+      viewIndex: 0
     })
-    if (resp.status === 200 && !hasError(resp)) {
-      const products = resp.data.response.docs;
+
+    if (!isError(resp)) {
+      const products = resp.products;
       // Handled empty response in case of failed query
-      if (resp.data) commit(types.PRODUCT_ADD_TO_CACHED_MULTIPLE, { products });
+      if (products) commit(types.PRODUCT_ADD_TO_CACHED_MULTIPLE, { products });
+    } else {
+      console.error(resp)
     }
     // TODO Handle specific error
     return resp;
