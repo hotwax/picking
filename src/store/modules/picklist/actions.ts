a09204a936599f0b8d419450dbc7d35fed275f1a
yogeshsakle <yogeshsakle8@gmail.com>
2022-10-14 14:36:32 +0530

    Task Done

--- src/store/modules/picklist/actions.ts
+++ src/store/modules/picklist/actions.ts
@@ -1,84 +1,87 @@
-import { ActionTree } from 'vuex'
-import RootState from '@/store/RootState'
-import PicklistState from './PicklistState'
-import * as types from './mutation-types'
-import { PicklistService } from '@/services/PicklistService'
-import { hasError, showToast } from '@/utils'
-import { translate } from '@/i18n'
+import { ActionTree } from "vuex";
+import RootState from "@/store/RootState";
+import PicklistState from "./PicklistState";
+import * as types from "./mutation-types";
+import { PicklistService } from "@/services/PicklistService";
+import { hasError, showToast } from "@/utils";
+import { translate } from "@/i18n";
 
 const actions: ActionTree<PicklistState, RootState> = {
   /**
    * Find picklist
    */
-  async findPickList ({ commit }) {
+  async findPickList({ commit }) {
     let resp;
 
     try {
       resp = await PicklistService.getPicklists();
 
       if (resp.status === 200 && resp.data.pickingList && !hasError(resp)) {
-        commit(types.PICKLISTS_UPDATED, { list: resp.data.pickingList })
+        commit(types.PICKLISTS_UPDATED, { list: resp.data.pickingList });
         return resp.data;
       } else {
-        showToast(translate('Something went wrong'));
+        showToast(translate("There are no picklists available"));
         console.error("error", resp.data._ERROR_MESSAGE_);
         return Promise.reject(new Error(resp.data._ERROR_MESSAGE_));
       }
     } catch (err) {
-      showToast(translate('Something went wrong'));
+      showToast(translate("Something went wrong"));
       console.error("error", err);
-      return Promise.reject(new Error(err))
+      return Promise.reject(new Error(err));
     }
   },
-  
+
   /**
    * Set current picklist data
    */
-  async setCurrentPicklist ({ commit, state }, payload) {
+  async setCurrentPicklist({ commit, state }, payload) {
     let resp;
 
-    const current = state.current as any
+    const current = state.current as any;
 
-    if (current.picklist && current.pickingItemList && current.picklist.picklistId === payload.id) {
+    if (
+      current.picklist &&
+      current.pickingItemList &&
+      current.picklist.picklistId === payload.id
+    ) {
       return {
-        'pickingItemList': current.pickingItemList
-      }
+        pickingItemList: current.pickingItemList,
+      };
     }
 
     try {
       resp = await PicklistService.getPicklist(payload);
       resp.data.pickingItemList.map((picklist: any) => {
         picklist.isChecked = false;
-      })
+      });
       if (resp.status === 200 && resp.data.pickingItemList && !hasError(resp)) {
-        commit(types.PICKLIST_CURRENT_UPDATED, { current: resp.data })
+        commit(types.PICKLIST_CURRENT_UPDATED, { current: resp.data });
         let productIds: any = new Set(
           resp.data.pickingItemList.map((picklist: any) => {
-            return picklist.productId
+            return picklist.productId;
           })
         );
-        productIds = [...productIds]
+        productIds = [...productIds];
         if (productIds.length) {
-          this.dispatch('product/fetchProducts', { productIds })
+          this.dispatch("product/fetchProducts", { productIds });
         }
         return resp.data;
       } else {
-        showToast(translate('Something went wrong'));
+        showToast(translate("Something went wrong"));
         console.error("error", resp.data._ERROR_MESSAGE_);
         return Promise.reject(new Error(resp.data._ERROR_MESSAGE_));
       }
-
     } catch (err) {
-      showToast(translate('Something went wrong'));
+      showToast(translate("Something went wrong"));
       console.error("error", err);
-      return Promise.reject(new Error(err))
+      return Promise.reject(new Error(err));
     }
   },
 
   /**
    * Complete Picklist
    */
-  async completePicklist ({ commit }, payload) {
+  async completePicklist({ commit }, payload) {
     let resp;
 
     try {
@@ -92,15 +95,15 @@ const actions: ActionTree<PicklistState, RootState> = {
     } catch (error) {
       console.log(error);
       showToast(translate("Something went wrong"));
-    } 
+    }
   },
 
   /*
   clearing the picklist before the user is logged out, because if some other user log in
   having zero picklist then it shows the previous picklist entries
   */
-  clearPicklist ({ commit }) {
-    commit(types.PICKLISTS_UPDATED, { list: {} })
-  }
-}
+  clearPicklist({ commit }) {
+    commit(types.PICKLISTS_UPDATED, { list: {} });
+  },
+};
 export default actions;
--- src/views/Picklist-Detail.vue
+++ src/views/Picklist-Detail.vue
@@ -5,88 +5,120 @@
         <ion-back-button default-href="/" slot="start" />
         <ion-title>{{ id }}</ion-title>
         <ion-buttons slot="end">
-          <ion-button @click="selectAll" >{{ $t ("Select all") }}</ion-button>
+          <ion-button @click="selectAll">{{ $t("Select all") }}</ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content>
       <ion-list>
-        <ion-item-group v-for="picklist in picklistGroup" :key="picklist.alphabet" >
+        <ion-item-group
+          v-for="picklist in picklistGroup"
+          :key="picklist.alphabet"
+        >
           <ion-item-divider>
             <ion-label> {{ picklist.alphabet }}</ion-label>
           </ion-item-divider>
-          <PicklistDetailItem :picklists="picklist.record"/>
+          <PicklistDetailItem :picklists="picklist.record" />
         </ion-item-group>
       </ion-list>
-     </ion-content>
+    </ion-content>
 
-     <ion-footer>
+    <ion-footer>
       <ion-toolbar>
         <ion-buttons class="footer-buttons">
-          <ion-button class="action-button" fill="outline" color="secondary" @click="scanCode()">
+          <ion-button
+            class="action-button"
+            fill="outline"
+            color="secondary"
+            @click="scanCode()"
+          >
             <ion-icon slot="start" :icon="barcodeOutline" />{{ $t("Scan") }}
           </ion-button>
-          <ion-button class="action-button"  @click="completeProductPicklist" fill="outline" color="success">
+          <ion-button
+            class="action-button"
+            @click="completeProductPicklist"
+            fill="outline"
+            color="success"
+          >
             <ion-icon slot="start" :icon="checkmarkDone" />{{ $t("Complete") }}
           </ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-footer>
- </ion-page>
+  </ion-page>
 </template>
 
 <script>
-import { IonBackButton, IonButton, IonButtons, IonContent, IonFooter, IonHeader, IonIcon, IonItemDivider, IonItemGroup, IonLabel, IonList, IonPage, IonTitle, IonToolbar, alertController, modalController } from '@ionic/vue';
-import { defineComponent } from 'vue';
-import { barcodeOutline, checkmarkDone } from 'ionicons/icons';
-import PicklistDetailItem from '@/components/Picklist-detail-item.vue';
-import { mapGetters, useStore } from 'vuex';
-import { translate } from '@/i18n'
-import { showToast } from '@/utils';
-import Scanner from '@/components/Scanner'
-import { useRouter } from 'vue-router';
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonIcon,
+  IonItemDivider,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  alertController,
+  modalController,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { barcodeOutline, checkmarkDone } from "ionicons/icons";
+import PicklistDetailItem from "@/components/Picklist-detail-item.vue";
+import { mapGetters, useStore } from "vuex";
+import { translate } from "@/i18n";
+import { showToast } from "@/utils";
+import Scanner from "@/components/Scanner";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
-  name: 'PicklistDetail',
+  name: "PicklistDetail",
   components: {
-    IonBackButton, 
-    IonButton, 
-    IonButtons, 
+    IonBackButton,
+    IonButton,
+    IonButtons,
     IonContent,
-    IonFooter, 
-    IonHeader, 
+    IonFooter,
+    IonHeader,
     IonIcon,
     IonItemDivider,
-    IonItemGroup, 
+    IonItemGroup,
     IonLabel,
     IonList,
-    IonPage, 
-    IonTitle, 
+    IonPage,
+    IonTitle,
     IonToolbar,
-    PicklistDetailItem
+    PicklistDetailItem,
   },
   computed: {
     ...mapGetters({
-      picklists: 'picklist/getPicklists',
-      picklistItem: 'picklist/getCurrent',
-    })
+      picklists: "picklist/getPicklists",
+      picklistItem: "picklist/getCurrent",
+    }),
   },
   data() {
     return {
-      picklistGroup: []
-    }
+      picklistGroup: [],
+    };
   },
-  props: ['id'],
-  async mounted () {
-    await this.store.dispatch('picklist/setCurrentPicklist', { id: this.id })
+  props: ["id"],
+  async mounted() {
+    await this.store.dispatch("picklist/setCurrentPicklist", { id: this.id });
     // Sort picklist products alphabetically
     // Used localeCompare to compare productName
-    this.picklistItem.pickingItemList.sort((a, b) => a.productName.localeCompare(b.productName, { sensitivity: 'base' }));
+    this.picklistItem.pickingItemList.sort((a, b) =>
+      a.productName.localeCompare(b.productName, { sensitivity: "base" })
+    );
     // Arrange picklist products alphabetically
     const data = this.picklistItem.pickingItemList.reduce((r, e) => {
       // To display "0th" index value i.e. first letter of product name
       const alphabet = e.productName[0];
-      if (!r[alphabet]) r[alphabet] = { alphabet, record: [e] }
+      if (!r[alphabet]) r[alphabet] = { alphabet, record: [e] };
       else r[alphabet].record.push(e);
       return r;
     }, {});
@@ -94,75 +126,84 @@ export default defineComponent({
   },
   methods: {
     async completePicklist() {
-      const picklistChecked = this.picklistItem.pickingItemList.some((picklist) =>
-        picklist.isChecked
-      )
+      const picklistChecked = this.picklistItem.pickingItemList.some(
+        (picklist) => picklist.isChecked
+      );
       const payload = {
-        'picklistId': this.picklistItem.picklist.picklistId,
-        'statusId': 'PICKLIST_PICKED'
-      }
+        picklistId: this.picklistItem.picklist.picklistId,
+        statusId: "PICKLIST_PICKED",
+      };
       if (picklistChecked) {
-        return this.store.dispatch("picklist/completePicklist", payload ).then(() => {
-          this.router.push("/tabs/picklists");
-        })
+        return this.store
+          .dispatch("picklist/completePicklist", payload)
+          .then(() => {
+            this.router.push("/tabs/picklists");
+          });
       } else {
         showToast(translate("Something went wrong"));
       }
     },
     async completeProductPicklist() {
-      const alert = await alertController
-        .create({
-          header: this.$t("Complete picklist"),
-          buttons: [
-            {
-              text: this.$t('Cancel'),
-              role: 'cancel'
+      const alert = await alertController.create({
+        header: this.$t("Complete picklist"),
+        buttons: [
+          {
+            text: this.$t("Cancel"),
+            role: "cancel",
+          },
+          {
+            text: this.$t("Complete"),
+            handler: () => {
+              if (
+                this.picklistItem.pickingItemList.some(
+                  (picklist) => picklist.isChecked
+                )
+              )
+                this.completePicklist();
+              else showToast(translate("No item has been picked"));
             },
-            {
-              text:this.$t('Complete'),
-              handler: () => {
-                if (this.picklistItem.pickingItemList.some((picklist) => picklist.isChecked)) this.completePicklist();
-                else showToast(translate("No item has been picked"));
-              },
-            },
-          ],
-        });
+          },
+        ],
+      });
       return alert.present();
     },
     selectAll() {
       this.picklistItem.pickingItemList.map((picklist) => {
         picklist.isChecked = true;
-      })
+      });
     },
     async scanCode() {
-      const modal = await modalController
-        .create({
-          component: Scanner,
-        });
-      modal.onDidDismiss()
-        .then((result) => {
-          //result : value of the scanned barcode/QRcode
-          const item = result.data.value && this.picklistItem.pickingItemList.find((product) => !product.isChecked && product.productId === result.data.value)
-          if (item) {
-            item.isChecked = true;
-          } else {
-            showToast(translate("Product not found"))
-          }
-        });
+      const modal = await modalController.create({
+        component: Scanner,
+      });
+      modal.onDidDismiss().then((result) => {
+        //result : value of the scanned barcode/QRcode
+        const item =
+          result.data.value &&
+          this.picklistItem.pickingItemList.find(
+            (product) =>
+              !product.isChecked && product.productId === result.data.value
+          );
+        if (item) {
+          item.isChecked = true;
+        } else {
+          showToast(translate("Product not found"));
+        }
+      });
       return modal.present();
-    }
+    },
   },
   setup() {
     const store = useStore();
     const router = useRouter();
-      
+
     return {
       barcodeOutline,
       checkmarkDone,
       store,
-      router
-    }
-  }
+      router,
+    };
+  },
 });
 </script>
 
