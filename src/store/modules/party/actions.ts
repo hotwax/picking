45e9db0dfb1b6e69afd424ca80f8fe6b238f3cbd
k2maan <ketuman333@gmail.com>
2023-04-21 18:46:16 +0530

    Fixed: operator parameter in params

--- src/store/modules/party/actions.ts
+++ src/store/modules/party/actions.ts
@@ -16,7 +16,7 @@ const actions: ActionTree<PartyState, RootState> = {
     const params = {
       "inputFields": {
         "partyId": unavailablePickersPartyIds,
-        "userPartyId_op": 'in'
+        "partyId_op": 'in'
       },
       "fieldList": ["firstName", "lastName", "partyId"],
       "entityName": "PartyNameView",
