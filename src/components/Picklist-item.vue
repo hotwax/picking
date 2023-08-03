f69627047a48d8bd960bea7c7585d245ba1bb008
Sanskar Soni <sonisanskar223@gmail.com>
2023-08-03 17:49:06 +0530

    Implemented: aligned date and arrow by wrapping ion-note with div tag

--- src/components/Picklist-item.vue
+++ src/components/Picklist-item.vue
@@ -4,7 +4,7 @@
       <h2>{{ picklist.pickersFullName }}</h2>
       <p>{{ picklist.picklistId }}</p>
     </ion-label>
-    <ion-note slot="end">{{ getTime(picklist.picklistDate) }}</ion-note>
+    <div><ion-note slot="end">{{ getTime(picklist.picklistDate) }}</ion-note></div>
   </ion-item>
 </template>
 
