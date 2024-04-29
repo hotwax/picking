9a63c74bef864c929765df5c8c7569c96d706426
R-Sourabh <rsourabh002@gmail.com>
2024-04-29 15:52:49 +0530

    Fixed: html tags not working in the translations (#691)

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,8 @@ import localeMessages from './locales';
 
 const app = createApp(App)
   .use(IonicVue, {
-    mode: 'md'
+    mode: 'md',
+    innerHTMLTemplatesEnabled: true
   })
   .use(router)
   .use(i18n)
